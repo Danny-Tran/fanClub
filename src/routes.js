cca07cee2ecffba3d1fb6d6df6b5feaf5f77469f
Chron0 <truong_tran@live.com>
2019-08-13 16:27:28 -0600

    created a home routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Layout from './HighOrderComponents/Layout'
-import { Switch, Route }
+import { Switch, Route } from 'react-router-dom'
+import Home from './Component/Home'
 
 const Routes = (props) => {
   return (
     <Layout>
-      
+      <Switch>
+        <Route exact component={Home} path='/'/>
+      </Switch>
     </Layout>
   )
 }
