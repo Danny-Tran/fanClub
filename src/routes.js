95c1a7470e4b4fb29284afc2c14812b81b932359
Chron0 <truong_tran@live.com>
2019-08-13 16:19:48 -0600

    creating home component with contain animations

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Layout from './HighOrderComponents/Layout'
+import { Switch, Route }
 
 const Routes = (props) => {
   return (
     <Layout>
-      second line
+      
     </Layout>
   )
 }
