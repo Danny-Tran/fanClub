447c9f67d012cff17c4691b61ff03a1b9f168afb
Chron0 <truong_tran@live.com>
2019-08-16 12:49:02 -0600

    creating home pages animation

--- src/Component/Home/Featured/index.js
+++ src/Component/Home/Featured/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Stripe from './Stripe'
 
 const Featured = () => {
   return (
     <div className='featured_wrapper'>
-      THIS IS INDEX FEATURE
+      <Stripe />
     </div>
   );
 };
