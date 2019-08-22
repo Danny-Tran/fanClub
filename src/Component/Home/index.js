156bcc31400031ac03eb5b2da96a0fe64ff7e14e
Chron0 <truong_tran@live.com>
2019-08-22 12:26:07 -0600

    creating animate numbers

--- src/Component/Home/index.js
+++ src/Component/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Featured  from '../Home/Featured'
+import Matches from '../Home/matches'
 
 const Home = () => {
   return (
     <div className='bck_blue'>
-      <Featured/>      
+      <Featured/>
+      <Matches/>      
     </div>
   );
 };
---
+++ src/Component/Home/matches/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MatchesHome = () => {
+  return (
+    <div className='home_matches_wrappers'>
+      <div className='container'>
+        TAG
+
+        BLOCK
+
+        TAG
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default MatchesHome;
