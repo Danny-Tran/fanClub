c63e3793f5bb055c50ebf525cbdeeeffbb649893
Chron0 <truong_tran@live.com>
2019-09-19 13:53:23 -0600

    meet the team button added

--- src/Component/Home/meetPlayers/index.js
+++ src/Component/Home/meetPlayers/index.js
@@ -22,10 +22,7 @@ class MeetPlayers extends Component {
 
             <div className='home_text_wrapper'>
               <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#ffffff"
+                <Tag bck="#0e1731" size="100px" color="#ffffff"
                   add={{
                     display:'inline-block',
                     marginBottom:'20px',
@@ -36,10 +33,7 @@ class MeetPlayers extends Component {
               </div>
 
               <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#ffffff"
+                <Tag bck="#0e1731" size="100px" color="#ffffff"
                   add={{
                     display:'inline-block',
                     marginBottom:'20px',
@@ -50,10 +44,7 @@ class MeetPlayers extends Component {
               </div>
 
               <div>
-                <Tag
-                  bck="#0e1731"
-                  size="100px"
-                  color="#ffffff"
+                <Tag bck="#0e1731" size="100px" color="#ffffff"
                   add={{
                     display:'inline-block',
                     marginBottom:'20px',
@@ -62,8 +53,18 @@ class MeetPlayers extends Component {
                   Players
                 </Tag>
               </div>
+              <div>
+                <Tag bck="#ffffff" size="27px" color="#0e1731" link={true} linkto='/the_team'
+                  add = {{
+                    display:'inline-block',
+                    marginBottom:'20px',
+                    border: '1px solid #0e1731'
+                  }}
+                >
+                  Meet Them Here
+                </Tag>
+              </div>
             </div>
-
           </div>
         </div>
       </div>
