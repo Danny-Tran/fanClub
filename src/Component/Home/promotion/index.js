a9f385f2a5e445fdf02eb35784f3446b233d0307
Chron0 <truong_tran@live.com>
2019-09-23 16:35:25 -0600

    creating enroll components

---
+++ src/Component/Home/promotion/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Promotion = () => {
+  return (
+    <div className='promotion_wrapper'
+      style={{background:'white'}}
+    >
+      <div className='container'>
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Promotion;
